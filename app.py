a4dabfb202f2756f69c7e2c1b3a20d3036b74f21
Rachit Keyal <rachitkeyal@gmail.com>
2024-09-14 03:39:56 -0400

    got carousel working

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return render_template('home.html')
+
+@app.route('/submit', methods=['GET','POST'])
+def submit():
+    # do the submission
+    ingredients = request.form['ingredients']
+    return redirect(url_for('recipes'))
+    # do this render_template with recipes information
+
+@app.route('/recipes')
+def recipes():
+    return render_template('recipes.html')
